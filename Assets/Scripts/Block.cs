3e908e31ea148fb70337bcef0d81572f60518f6d
dariaveremey <daria.veremey.v@gmail.com>
2022-06-28 18:29:00 +0300

    no message

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -4,7 +4,6 @@ public class Block : MonoBehaviour
 {
     #region Viriables
 
-    private int _numberStrikes;
     [SerializeField] private int _numberDestroy;
     [SerializeField] private int _points;
 
@@ -18,6 +17,7 @@ public class Block : MonoBehaviour
     private Sprite _secondChangedSpriteRenderer;
 
     private int _pointsNumber;
+    private int _numberStrikes;
 
     #endregion
 
@@ -30,27 +30,35 @@ public class Block : MonoBehaviour
 
         if (_numberStrikes == _numberDestroy)
         {
-            _pointsNumber += _points;
+            //_pointsNumber += _points;
             Destroy(gameObject);
             Debug.Log($"{_pointsNumber}");
         }
 
         if (_numberStrikes == 1)
         {
-            _firstChangedSpriteRenderer = Sprite.Create(_firstDestroySpriteTexture2D,
-                new Rect(0.0f, 0.0f, _firstDestroySpriteTexture2D.width, _firstDestroySpriteTexture2D.height),
-                new Vector2(0.5f, 0.5f), _firstCoefficient);
-            GetComponent<SpriteRenderer>().sprite = _firstChangedSpriteRenderer;
+            Convent(_firstChangedSpriteRenderer, _firstDestroySpriteTexture2D, _firstCoefficient);
         }
 
         if (_numberStrikes == 2)
         {
-            _secondChangedSpriteRenderer = Sprite.Create(_secondDestroySpriteTexture2D,
-                new Rect(0.0f, 0.0f, _secondDestroySpriteTexture2D.width, _secondDestroySpriteTexture2D.height),
-                new Vector2(0.5f, 0.5f), _secondCoefficient);
-            GetComponent<SpriteRenderer>().sprite = _secondChangedSpriteRenderer;
+            Convent(_secondChangedSpriteRenderer, _secondDestroySpriteTexture2D, _secondCoefficient);
         }
     }
 
     #endregion
+
+
+    #region Private methods
+    
+
+    #endregion
+    private void Convent(Sprite changeSprite, Texture2D currentTexture2D, float coefficient)
+    {
+        changeSprite = Sprite.Create(currentTexture2D,
+            new Rect(0.0f, 0.0f, currentTexture2D.width, currentTexture2D.height),
+            new Vector2(0.5f, 0.5f), coefficient);
+        GetComponent<SpriteRenderer>().sprite = changeSprite;
+    }
+
 }
